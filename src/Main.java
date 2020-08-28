0e4f9499bd551bc824287567dbf553ad79faa964
신효원 <a01055327595@gmail.com>
2020-08-28 15:27:32 +0000

    third variable try

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String args[]) {
-        int x = 5; 
-        System.out.println(x);
-        
-        x=10; //변수 x에 10을 저장, 기존의 값은 사라짐
-        System.out.println(x);
+        int x = 10;
+        int y = 5;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
     }
 }
