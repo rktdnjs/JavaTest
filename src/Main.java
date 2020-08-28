493e3b9dbd38099ba1383781e9bd87e5d4aea3bc
신효원 <a01055327595@gmail.com>
2020-08-28 15:36:07 +0000

    variable

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String args[]) {
-        int x = 10;
-        int y = 5;
-        System.out.println(x+y);
-        System.out.println(x-y);
-        System.out.println(x*y);
-        System.out.println(x/y);
+        /*변수를 선언할 때 변수에 저장할 겂의 종류에 다라 변수의 타입을 선택해야한다.
+        변수의 타입은 참조형과 8개의 기본형이 있다. 그 중 4개의 타입만 알아도 프로그래밍을 하는데 큰 지장은 없다.
+        int x, double pi(실수형태 double은 15자리), char ch = 'a'(문자 1개 형태), String str = "abc" (여러문자, 즉 0~n개를 저장)*/
+        int x = 5;
+        double pi = 3.14;
+        char ch = 'a';
+        String str = "abc";
+        
+        System.out.println(x);
+        System.out.println(pi);
+        System.out.println(ch);
+        System.out.println(str);
+        
     }
 }
