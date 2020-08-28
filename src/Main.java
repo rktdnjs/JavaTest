b8182acb599a36ad40c17173e16db263aef3bba9
신효원 <a01055327595@gmail.com>
2020-08-28 15:23:39 +0000

    second

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String args[]) {
-        System.out.println(5+3);
-        System.out.println(5-3);
-        System.out.println(5*3);
-        System.out.println(5/3);
+        int x = 5; 
+        System.out.println(x);
+        
+        x=10; //변수 x에 10을 저장, 기존의 값은 사라짐
+        System.out.println(x);
     }
 }
