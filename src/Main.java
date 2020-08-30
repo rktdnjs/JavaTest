2cd1aebd1ef33622f5819f607e8fd443cfc3db01
신효원 <rktdnjs@naver.com>
2020-08-30 13:46:10 +0000

    primitive type, reference type

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 public class Main {
     public static void main(String args[]) {
-        /* 두 변수의 값 바꾸기, 55
-        두 변수 x,y에 저장된 값을 바꾸기 위해서는 x의값을 y에저장하고, y의값을 x에저장해서는 원하는 결과를 얻을수 x
-        두 컵에 담긴 내용물을 바꾸려면 빈 컵이 필요한 것처럼, 값을 임시로 저장할 변수가 하나 더 필요하다.
-        int tmp; (임시로 값을 저장하기 위한 변수, 빈 컵 역할)
+        /* 기본형과 자료형 56
+        자료형에는 문자형(char), 정수형(byte, short, int, long), 실수형(float, douoble) 등.
+        변수를 선언할때는 저장하려는 값의 특성을 고려하여 가장 알맞은 자료형을 변수의 타입으로 선택하면 된다.
         
-        tmp = x;  (x의값을 tmp에 저장)
-        x = y;  (y의값을 x에 저장)
-        y = tmp;  (tmp에 저장된 값을 y에 저장)
+        자료형은 크게 기본형과 참조형 2가지로 나눌 수 있다. 기본형 변수는 실제 값을 저장하는 반면
+        참조형 변수는 어떤 값이 저장되어 있는 주소를 값으로 갖는다.
+        자바는 C언어와 달리 참조형 변수 간의 연산을 할 수 ㅇ벗으므로 실제 연산에 사용되는 것은 모두 기본형 변수이다.
+        
+        기본형(primitive type)
+        논리형(boolean), 문자형(char), 정수형(byte, short, int, long), 실수형(float, double) 계산을 위한 실제 값을 저장한다.
+        참조형(reference type)
+        객체의 주소를 저장한다. 8개의 기본형을 제외한 나머지 타입.
         */
         
-        int x = 10; int y = 5;
-        System.out.println("x="+x);
-        System.out.println("y="+y);
-        
-        int tmp = x; // x의 값을 tmp에 저장
-        x = y;  //y의 값을 x에 저장
-        y = tmp;  //tmp에 저장된 값을 y에 저장
-        System.out.println("x="+x);
-        System.out.println("y="+y);
     }
 }
