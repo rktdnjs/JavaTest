3dafb6c676474a83f66be3fd243700916fe61bc6
신효원 <rktdnjs@naver.com>
2020-09-02 14:10:29 +0000

    printf 5

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,15 @@ public class Main {
         %f : 부동소수점의 형식으로 출력 / %c : 문자 출력 / %s : 문자열로 출력
         */
         
+        /*
         String url = "www.naver.com";
         float f1 =.10f;  //0.10, 1.0e-1
         float f2 = 1e1f; //10.0, 1.0e1, 1.0e1+1
         float f3 = 3.14e3f;
+        */
         double d = 1.23456789;
         
+        /*
         //%e : 지수형태로 출력할 때 실수 지시자, %g : 값을 간략하게 표현할 때 실수 지시자.
         //%f는 기본적으로 소수점 아래 6자리 까지만 출력한다. 7자리부터는 반올림한다.
         System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
@@ -26,6 +29,10 @@ public class Main {
         System.out.printf("[%-20s]%n", url); //왼쪽 정렬, 20글자
         System.out.printf("[%.8s]%n", url); //왼쪽에서 8글자만.
         //.(숫자)s : 왼쪽에서 (숫자)까지 만 출력
+        */
+        
+        System.out.printf("d=%-20f%n", d);
+        System.out.printf("d=%.5f%n", d);
         
         
         
