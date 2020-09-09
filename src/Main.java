8a50ea185a025d071a5ba39573f7023ddf423379
신효원 <rktdnjs@naver.com>
2020-09-09 14:21:24 +0000

    operator 74

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,24 @@ public class Main {
     public static void main(String args[]) {
         /* 73
         연산자의 결합규칙.
-        이때 단항 연산자와 대입 연산자는 진행방향이 오른쪽에서 왼쪽.
-        단항 연산자의 경우  -x 일 때 x의 부호를 먼저 바꾸고, 이후 계산이 수행.
-        대입 연산자의 경우  x=y=10 일때 y에 우선대입후 x에 대입이 됨.
-        그 외에는 진행방향이 왼쪽에서 오른쪽.
+        증감 연산자는 피연산자의 값을 1증가 또는 감소시킨다.
+        증감 연산자는 피연산자의 어디에 위치하느냐에 따라 살짝 다른데
+        피연산자의 왼쪽이면 전위형(prefix), 오른쪽이면(postfix)
+        전위형 : 값이 참조되기 전에 증가.   j=++i
+        후위형 : 값이 참조된 후에 증가.     j=i++
         */
         
-        int x,y;
-        x=y=3;
-        System.out.println("x=" + x);
-        System.out.println("y=" + y);
+        int i=5;
+        int j=0;
+        
+        j=i++;
+        System.out.println("j=i++; 실행 후, i=" + i +", j="+ j);
+        
+        i=5; // 결과를 비교하기 위해, i와j의 값을 다시 5와0으로 변경
+        j=0;
+        
+        j=++i;
+        System.out.println("j=++i; 실행 후, i=" + i +", j="+ j);
         
     }
 }
