bbfad1a5ba7863a0cd9e048372fe4b4cc68e2394
신효원 <rktdnjs@naver.com>
2020-09-10 13:32:53 +0000

    operator 78

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        /* 76
-        부호 연산자.
-        부호 연산자는 boolean형과 char형을 제외한 기본형에만 쓸 수 있다.
-        피연산자의 부호를 반대로 변경한 결과를 반환한다.
+        /* 77
+        형변환 연산자(casting operator)
+        서로 다른 타입 간의 연산을 수행해야 하는 경우, 연산을 수행하기 전에 타입을 일치시켜야 한다.
+        변수나 리터럴의 타입을 다른 타입으로 변환하는 것을 형변환이라고 한다.
+        (타입)피연산자
+        이때 사용되는 괄호는 '캐스트 연산자' 또는 '형변환 연산자'라고 한다.
         */
         
-        int i = -10;
-        i = +i;
-        System.out.println(i);
+        double d = 97.4;
+        int score = (int) d;
+        System.out.println("score=" + score);
+        System.out.println("d=" + d);
         
-        i = -10;
-        i = -i;
-        System.out.println(i);
+        //출력한 후에도 피연산자 d에는 변화값이 없다.
     }
 }
