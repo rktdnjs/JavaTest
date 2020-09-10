9c8e93021a44984800fc35627070556026751427
신효원 <rktdnjs@naver.com>
2020-09-10 13:27:09 +0000

    operator 76

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        /* 73
-        연산자의 결합규칙.
-        증감 연산자는 피연산자의 값을 1증가 또는 감소시킨다.
-        증감 연산자는 피연산자의 어디에 위치하느냐에 따라 살짝 다른데
-        피연산자의 왼쪽이면 전위형(prefix), 오른쪽이면(postfix)
-        전위형 : 값이 참조되기 전에 증가.   j=++i
-        후위형 : 값이 참조된 후에 증가.     j=i++
+        /* 76
+        부호 연산자.
+        부호 연산자는 boolean형과 char형을 제외한 기본형에만 쓸 수 있다.
+        피연산자의 부호를 반대로 변경한 결과를 반환한다.
         */
         
-        int i=5;
-        int j=5;
-        System.out.println(i++); //i의 값을 출력후, 1증가
-        System.out.println(++j); //j의 값을 1증가후, 출력
-        System.out.println("i= "+i+ ", j= "+j);
+        int i = -10;
+        i = +i;
+        System.out.println(i);
+        
+        i = -10;
+        i = -i;
+        System.out.println(i);
     }
 }
