a91562b1b408805b59ebdd06548d4f2d6f60e410
신효원 <rktdnjs@naver.com>
2020-09-19 09:09:09 +0000

    operator 84

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,10 @@ public class Main {
         /* 84 그외 나머지 연산자
         
         */
-        int x, y;
-        x = 10;
-        y = 8;
-        
-        System.out.printf("%d를 %d로 나누면, %n", x, y);
-        System.out.printf("몫은 %d이고, 나머지는 %d입니다.%n", x/y , x%y);
+        System.out.println(-10%8);
+        System.out.println(10%-8);
+        System.out.println(-10%-8);
+        //나머지를 나타내는 연산자(%)는 나누는 수로 음수도 가능하다. 다만, 부호를 무시한다는 점만 기억하자.
         
     }
 }
