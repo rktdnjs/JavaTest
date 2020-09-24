4561695f80c8799a0f6930009c07919b334f622d
신효원 <rktdnjs@naver.com>
2020-09-24 14:22:50 +0000

    if else statement another version

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         
         System.out.println("숫자를 하나 입력하세요");
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt(); //화면을 통해 입력받은 숫자를 input에 저장
+        int input;
+        input = sc.nextInt(); //화면을 통해 입력받은 숫자를 input에 저장
         
         if(input==0)
         {
