7916261e5be42053a0d7f27fb18aa609057d325c
신효원 <rktdnjs@naver.com>
2020-09-25 14:03:09 +0000

    if statement 105

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* if-else if문 103p
+        /* if 중첩문 104p
         
         */
-        int score = 0; //점수를 저장하기 위한 변수
-        char grade = ' '; //학점을 저장하기 위한 변수
         
-        System.out.print("점수를 입력해보세요 >^< >>");
+        //점수에따라 학점을 출력하는 프로그램
+        
+        int score;
+        char grade = ' ', opt = '0';
+        
+        System.out.print("점수를 입력해주세요 >ㅇ< >>");
         Scanner sc = new Scanner(System.in);
-        score = sc.nextInt(); //입력받은 숫자를 score에 저장
+        score = sc.nextInt(); //화면을 통해 입력받은 점수를 score에 정수형으로 저장.
         
-        if(score >= 90){
+        System.out.printf("당신의 점수는 %d 입니다.%n", score);
+        
+        if(score>=90){  //90점이상 A, 95점이상 A+
             grade = 'A';
-            System.out.println("앗!!!! 당신은 과탑에 한 걸음 가까이 다가가셨습니다...good");
+            if(score>=95){
+                opt = '+';
+            } else if (score<94) {
+                opt = '-';
+            }
         }
-        else if(score >= 80){
+        
+        else if(score>=80){ //80점이상 B, 85점이상 B+
             grade = 'B';
+            if(score>=85){
+                opt = '+';
+            } else if (score<84) {
+                opt = '-';
+            }
         }
-        else if(score >= 70){
+        
+        else {
             grade = 'C';
         }
-        else if(score >= 60){
-            grade = 'D';
-        }
-        else{
-            grade = 'F';
-            System.out.println("앗!!! 축하드립니다....재수강이에요 >^<");
-        }
-        System.out.println("앗!!! 당신의 학점은" + grade + "입니다.");
+        
+        System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
         }
 	}
