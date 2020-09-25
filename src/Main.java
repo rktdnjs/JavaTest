be4d4200fe736267aa7548d49b96b9a4f647c8a6
신효원 <rktdnjs@naver.com>
2020-09-25 14:18:34 +0000

    switch statement 106

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* if 중첩문 104p
+        /* switch 문 106p
+        if문은 조건식의 결과가 참 거짓 둘뿐이라 경우의 수가 많아질수록 조건식이 많아져서 복잡해지고 처리시간도 길어진다.
+        이와 달리 switch문은 단 하나의 조건식으로 많은 경우의 수를 처맇라 수 있고 표현도 간결해 알아보기 쉽다.
+        다만 switch문은 제약조건이 있기 때문에 경우의 수가 많아도 어쩔 수 없이 if문으로 작성해야 하는 경우가 있다.
+        switch문의 순서.
+        1.조건식 계산, 2.조건식의 결과와 일치하는 case문으로 이동
+        3.이후의 문장들 수행, 4.break문이나 switch문의 끝을 만나면 switch문의 전체를 빠져나간다.
         
+        조건식의 결과와 일치하는 case문이 하나도 없을 경우에는 default문으로 이동한다.
+        default문은 보통 맨 마지막에 위치하기 때문에 생략해도 별 지장이 없다.(break문의 위치는 상관 x)
+        
+        switch문에서 break문은 각 case문의 영역을 구분하는 역할을 하는데, 만일 break문을 생략하면 case문 사이의 구분이 없어지므로
+        다른 break문을 지나거나 switch문 블럭{}의 끌을 만날때까지 나오는 모든 문장들을 수행한다.
+        그렇기 때문에 각 case문의 마지막에 break를 써서 끊어주는 것을 유의하도록 하자.
         */
         
-        //점수에따라 학점을 출력하는 프로그램
-        
-        int score;
-        char grade = ' ', opt = '0';
-        
-        System.out.print("점수를 입력해주세요 >ㅇ< >>");
-        Scanner sc = new Scanner(System.in);
-        score = sc.nextInt(); //화면을 통해 입력받은 점수를 score에 정수형으로 저장.
-        
-        System.out.printf("당신의 점수는 %d 입니다.%n", score);
-        
-        if(score>=90){  //90점이상 A, 95점이상 A+
-            grade = 'A';
-            if(score>=95){
-                opt = '+';
-            } else if (score<94) {
-                opt = '-';
-            }
-        }
-        
-        else if(score>=80){ //80점이상 B, 85점이상 B+
-            grade = 'B';
-            if(score>=85){
-                opt = '+';
-            } else if (score<84) {
-                opt = '-';
-            }
-        }
-        
-        else {
-            grade = 'C'; //나머지는 그냥 C ㅎㅎ 재수강 ㄱ
-        }
-        
-        System.out.printf("당신의 학점은 %c%c입니다.%n", grade, opt);
         }
 	}
