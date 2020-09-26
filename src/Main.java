927a5e42b2bfa6f25907727ad34d285d8931aef4
신효원 <rktdnjs@naver.com>
2020-09-26 13:54:38 +0000

    switch statement 108

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,32 @@
-//import java.util.*;
+import java.text.BreakIterator;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* switch 문 107p
-        switch문의 조건식은 결과값이 반드시 정수이어야 하며, 이 값과 일치하는 case문으로 이동하기 때문에
-        case문의 값 역시 정수이어야 한다. 그리고 중복되지 않아야 한다.
-        같은 값의 case문이 여러개이면, 어디로 이동해야할 지 알 수 없기 때문이다.
-        거기다가 case문의 값은 반드시 상수이어야 한다. 변수나 실수는 case문의 값으로 사용할 수 없다.
-        
-        [switch문의 제약조건]
-        1.switch문의 조건식 결과는 정수 또는 문자열이어야 한다.
-        2.case문의 값은 정수 상수(문자포함), 문자열만 가능하며, 중복되지 않아야 한다.
+        /* switch 문 108p
+        예제 만들기
         */
         
-        //예시
+        //입력되는 숫자값에따라 계절을 출력해주는 프로그램.
         
-        /*
-        int num, result;
-        final int ONE = 1; //상수
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
         
-        switch(result){
-            case '1': //문자리터럴
-            case ONE: //정수 상수
-            case "YES": //문자열리터럴. JDK1.7부터 가능
-            case num: //에러. 변수는 불가능
-            case 1.0: //에러. 실수도 불가능
-            */
-                
+        switch(month) {
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("현재 계절은 봄입니다. ^-^");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("현재 계졀은 여름입니다. ^-^;;");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("현재 계절은 가을입니다. ^-^....");
+            default:
+          //case 12: case 1: case 2:
+                System.out.println("현재 계절은 겨울~~입니다. ^~^");
+        }
+        
         }
 	}
