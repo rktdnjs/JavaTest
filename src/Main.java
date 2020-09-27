ba9f0eee54e2844d06c828e34a18b53613df1f99
신효원 <rktdnjs@naver.com>
2020-09-27 13:22:02 +0000

    for statement 112

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* for 문, 110page
+        /* for 문, 112page
         반복문의 종류로는 for과 while문, do-while문이있다.
         for과 while은 구조와 기능이 유사하여 어느 경우에나 서로 변환이 가능하며, 반복 횟수를 알고 있을 때는 for문을
         그렇지 않을 때는 while문을 사용한다.
@@ -12,9 +12,12 @@ public class Main {
         조건식이 참인 동안 블럭{}내의 문장들을 반복하다 거짓이 되면 반복문을 벗어난다.
         */
         
-        for (int i = 1; i <=5; i++) {
-            System.out.println("TFT mobile dudung deung jang");
-        }
+        for(int i =1; i<=5; i++)
+            System.out.println(i);
         
+        for(int i=1; i<=5; i++)
+            System.out.print(i);
+        
+        System.out.println(); //공백 출력후 줄 바꿈
         }
 	}
