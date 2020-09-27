dd8955823ca7baa932152d2785bbb5bee708594a
신효원 <rktdnjs@naver.com>
2020-09-27 13:32:43 +0000

    superposition for statement 113

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,10 @@ public class Main {
         별 찍기로 설명하기 아주 좋다.
         */
         
-        //10개의 별 5줄로 찍기 ver.1
+        //10개의 별 5줄로 찍기 ver.2
         for(int i=1; i<=5; i++) {
-            for(int j=1; j<=10; j++) {
-                System.out.print("*");
+                System.out.print("**********");
+                System.out.println();
             }
-            System.out.println();
-        }
-        
         }
 	}
