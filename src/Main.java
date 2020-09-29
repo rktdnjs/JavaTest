4f000b1575c71b3b191ca1dcc3e87ba37ce9601b
신효원 <rktdnjs@naver.com>
2020-09-29 14:04:27 +0000

    do-while statement 118

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* while 문
-        for문에 비해 while문은 구조가 간단하다.
-        if문처럼 조건식과 블럭{} 만으로 이루어져 있다.
-        다만 while문은 조건식이 참인동안 계속 반복하는 점을 유의하자.
+        /* do while 문, 118page
+        do-while문은 while문의 변형으로, 기본적인 구조는 while문과 같으나 조건식과 블럭{}의 순서를 바꿔놓은것이다.
+        그래서 while문과 반대로 블럭{}을 먼저 수행한 후에 조건식을 평가한다.
+        do-while은 최소한 한번은 블럭이 수행된다.
+        
+        그리많이 쓰이지는 않지만 반복적으로 사용자의 입력을 받아서 처리할 때 유용하다.
         */
         
-        //while문 예제, 입력받은 숫자의 각 자리의 합을 구하는 프로그램.
+        int input = 0, answer = 0;
         
-        int num = 0, sum = 0;
-        System.out.print("숫자를 입력해보세요 ㅎㅎ. >> ");
-        
+        answer = (int)(Math.random()*100) + 1; //1~100사이의 임의의 수를 저장, 원래 범위는 0<=Math.random<1이다. 계산하면 1<=Math.random<101이므로 1~100.
         Scanner sc = new Scanner(System.in);
-        String tmp = sc.nextLine(); //화면을통해 입력받은 내용을 tmp에 저장.
-        num = Integer.parseInt(tmp); //입력받은 문자열을 숫자로 변환.77
         
-        System.out.printf("num=%3d%n", num); //테스트용....%3d의 경우 최소 3글자까지 공백처리를 해서 출력해주는 의미이다.
-        
-        while(num!=0) {
-            sum += num%10; //num을 10으로 나눈 나머지를 sum에 더한다. 그렇게 계속 더한다.
-            System.out.printf("sum=%3d num=%d%n", sum, num);
+        do {
+            System.out.print("1과 100사이의 정수를 입력해보세요!! >>");
+            input = sc.nextInt();
             
-            num /= 10; //num = num / 10; num을 10으로 나눈 값을 다시 num에 저장한다.
-        }
+            if (input > answer) {
+                System.out.println("더 작은수로 다시 시도해보세요.");
+            } else if (input < answer) {
+                System.out.println("더 큰 수로 다시 시도해보세요.");
+            }
+        } while(input!=answer);
+        
+        System.out.println("정답입니다!! 짞짞짞 ^-^");
         }
     
 	}
