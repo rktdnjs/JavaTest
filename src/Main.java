90e960d694ba039f654dd1d9f0a405454d582cb3
신효원 <rktdnjs@naver.com>
2020-09-30 14:17:30 +0000

    gu gu dan program 122

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,17 @@ public class Main {
         */
         
         //for 문의 이름은 Loop1 이라 지정.
+        //구구단 출력 프로그램.
+        //출력할 때 2*1 ~ 9*9까지 나온다.
+        //곱하는 수가 10이 되면 break가 발생하여 근접한 반복문인 for(int~)를 벗어나 Loop1으로 돌아간다.
+        
         Loop1 : for(int i=2; i<=9; i++) {
             for(int j=1; j<=9; j++) {
-                if(j==5)
-                    break Loop1;
-                //break; 단순히 break 사용 시 제일 근접한 for문만 벗어난다.
+                if(j==10)
+                    //break Loop1;
+                break; //단순히 break 사용 시 제일 근접한 for문만 벗어난다.
                 //continue Loop1; => Loop1 반복문으로 벗어나 다시 실행. 이런 문장은 거의 쓸일이 없을 것.
-                //continue; => 바로 반복문의 끝으로 가서 다시 반복문 실행.
+                //continue; => 바로 (근접한)반복문의 끝으로 가서 다시 반복문 실행.
                 System.out.println(i+"*"+j+"*"+ i*j);
             }
             System.out.println();
