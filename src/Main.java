a765de304af67e099ad2855bb7ddcbb821109c66
신효원 <rktdnjs@naver.com>
2020-09-30 13:49:49 +0000

    continue statement

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* break문 119page
-        반복문에서도 break문을 쓸 수 있는데, break문은 자신이 포함된 가장 가까운 반복문을 벗어난다.
-        주로 if문과 함께 사용되어 특정 조건을 만족할 때 반복문을 벗어나게 한다.
+        /* continue 문, 120page
+        continue문은 반복문 내에서만 사용될 수 있으며, 반복이 진행되는 도중에 continue문을
+        만나게 되면 반복문의 끝으로 이동하여 다음 반복으로 넘어간다.
+        
+        continue문은 반복문 전체를 벗어나지 않고 다음 반복을 계속 수행한다는 점이 break문과 다르다.
+        전체 반복 중에 특정 조건을 만족하는 경우를 제외하고자 할 때 유용하다.
         */
         
-        //몇까지 더해야 100을 넘지 않는가?
-        int sum = 0;
-        int i =0;
-        
-        while (true) { //여기서 sum의 값이 100이 넘어서 break문이 실행 될 시 while문을 벗어난다.
-            if (sum > 100)
-                break;
-            ++i;
-            sum += i; // sum = sum + i
+        //3으로 나눠지지 않는 수들을 배열할 때.
+        for(int i = 0; i <= 10; i++){
+            if(i%3==0) 
+                continue; //조건식이 참이 되어 continue문이 수행되면 블럭의 끝으로 이동한다. break와 달리 반복문을 벗어나지 않는다.
+            System.out.println(i);
         }
-        System.out.println("i=" + i);
-        System.out.println("sum=" + sum);
         
         }
     
