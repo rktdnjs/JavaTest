8ab03a7908f45104f5b4b94a6d3abde10466d81a
신효원 <rktdnjs@naver.com>
2020-10-01 14:06:27 +0000

    printing example 124

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,57 @@ import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
-        /* 이름붙은 반복문, 122page
-        break는 근접한 단 하나의 반복문만 벗어날 수 있기 때문에, 여러개의 반복문이 중첩된 경우에는
-        break문으로 중첩 반복문을 완전히 벗어날 수 없다. 이럴 때는 중첩 반복문 앞에 이름을 붙이고 break문과 continue문에 이름을
-        지정해 줌으로써 하나 이상의 반복문을 벗어나거나 반복을 건너뛸 수 있다.
+        /* 
+        이름붙은 반복문.
         */
         
-        //for 문의 이름은 Loop1 이라 지정.
-        //구구단 출력 프로그램.
-        //출력할 때 2*1 ~ 9*9까지 나온다.
-        //곱하는 수가 10이 되면 break가 발생하여 근접한 반복문인 for(int~)를 벗어나 Loop1으로 돌아간다.
+        int menu = 0, num = 0;
+        Scanner sc = new Scanner(System.in);
         
-        Loop1 : for(int i=2; i<=9; i++) {
-            for(int j=1; j<=9; j++) {
-                if(j==10)
-                    //break Loop1;
-                break; //단순히 break 사용 시 제일 근접한 for문만 벗어난다.
-                //continue Loop1; => Loop1 반복문으로 벗어나 다시 실행. 이런 문장은 거의 쓸일이 없을 것.
-                //continue; => 바로 (근접한)반복문의 끝으로 가서 다시 반복문 실행.
-                System.out.println(i+"*"+j+"*"+ i*j);
+        outer: //while문에 outer라는 이름을 붙인 것
+        while(true) {
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.print("원하는 메뉴(1~3)를 선택하세요.(종료:0) >>");
+            
+            String tmp = sc.nextLine(); //화면에서 입력받은 내용을 tmp에 저장
+            menu = Integer.parseInt(tmp); //입력받은 문자열을 숫자로 변환
+            
+            if(menu==0) {
+                System.out.println("프로그램을 종료합니다.");
+                break; //while문 탈출
             }
-            System.out.println();
+            
+            else if (!(1<= menu && menu <=3)) {
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)");
+                continue; //while문의 끝으로 스킵한 후 다시 while문 반복.
             }
-        }
+            
+            for(;;) {
+                System.out.print("계산할 값을 입력하세요.(계산 종료 :0, 전체 종료:99)>>");
+                tmp = sc.nextLine(); 
+                num = Integer.parseInt(tmp);
+                
+                if(num==0)
+                    break; //계산종료. for문을 벗어난다.
+                
+                if(num==99)
+                    break outer; //전체종료. for문과 while문을 모두 벗어난다.
+                
+                switch(menu) {
+                    case 1:
+                        System.out.println("result="+ num*num);
+                        break;
+                    case 2:
+                        System.out.println("result="+ Math.sqrt(num));
+                        break;
+                    case 3:
+                        System.out.println("result="+ Math.log(num));
+                        break;
+                }
+            } //for(;;)의 끝
+        } //while의 끝
+        
+        } //main의 끝
 	}
