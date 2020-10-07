5de6b84e3608258fe6bd13694c7792df19dad2a1
신효원 <rktdnjs@naver.com>
2020-10-07 13:34:34 +0000

    2739 BAEKJOON 1try

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int a,b;
-        a = sc.nextInt();
-        b = sc.nextInt();
+        int N;
+        N = sc.nextInt();
         
-        if (b<45) {
-            if(a==0) {
-                a = 23;
-                b = 60-(45-b);
-                System.out.print(a+" "+b);
-            }
-            else{
-            a = a-1;
-            b = 60-(45-b);
-            System.out.print(a+" "+b);
-            }
-        }
-        
-        else { //(b>=45)
-            b = b-45;
-            System.out.print(a+" "+b);
+        for(int i=1; i<=9; i++) {
+            System.out.println(N*i);
         }
     }
 }
