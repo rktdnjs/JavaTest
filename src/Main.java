a75d75ddbc0f96efb2a2ce68ffd89baf33d78f88
신효원 <rktdnjs@naver.com>
2020-10-07 13:56:18 +0000

    10950 BAEKJOON example

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
+//각 테스트 케이스 마다 A+B를 출력한다.
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int N;
-        N = sc.nextInt();
-        
-        for(int i=1; i<=9; i++) {
-            System.out.println(N+"*"+i+"="+N*i);
+        int T, A, B;
+        T = sc.nextInt(); //시도하는 횟수
+        for(int i=1; i<=T; i++ ) {
+            A = sc.nextInt();
+            B = sc.nextInt();
+            System.out.println(A+B);
         }
     }
 }
